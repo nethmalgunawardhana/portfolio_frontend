b9f3eb798ee1cd48c5c871305dc8a8192a399e04
nethmalgunawardhana <nethmalgunawardhana@gmail.com>
2024-05-15 12:43:34 +0530

    new code

--- app.vue
+++ app.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="hello">
-    Hello Nuxt {{ version }}!
+  <div>
+   <h1>{{ name }}</h1>
+   <div v-if="pending">Loading... </div>
+   <h1>Projects</h1>
+   <ul>
+    <li v-for="project in projects" :key="project.name">
+
+      <h2> {{ project.name}} </h2>
+      <p>{{ project.description }}</p>
+    </li>
+   </ul>
+  </div>
+  <div>
+    <h1>Blogs</h1>
+    <ul>
+      <li v-for="blog in blogs" :key="blog.title">
+        <h2>{{ blog.title }}</h2>
+        <p>{{ blog.content }}</p>
+      </li>
+    </ul>
   </div>
 </template>
+
+
+<script setup>
+  const name ='NETHMAL GUNAWARDHANA';
+  const { data: projects,pending, error }=useFetch('http://localhost:5000/projects');
+
+  const {data: blogs,pending: pendingBlogs,error: errorBlogs,} = useFetch("http://localhost:5000/blogs");
+</script>
+
+<style></style>
+
