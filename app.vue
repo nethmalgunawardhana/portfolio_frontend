73f960400af0ba41698591b88887e4eb738c22e6
nethmalgunawardhana <nethmalgunawardhana@gmail.com>
2024-05-29 19:15:58 +0530

    create form to input data

--- app.vue
+++ app.vue
@@ -20,6 +20,22 @@
       </li>
     </ul>
   </div>
+  <div>
+<h1>Create a New Project</h1>
+<form @submit.prevent="createProject">
+<div>
+  <label for="name">Project Names:</label>
+  <input type="text" v-model="newProject.name" id="name" required/>
+</div>
+<div>
+  <label for="description">Project Names:</label>
+  <textarea  v-model="newProject.description" id="description" required></textarea>
+</div>
+
+<button type="submit">Create Project</button>
+
+</form>
+</div>
 </template>
 
 
@@ -28,6 +44,37 @@
   const { data: projects,pending, error }=useFetch('http://localhost:5000/projects');
 
   const {data: blogs,pending: pendingBlogs,error: errorBlogs,} = useFetch("http://localhost:5000/blogs");
+
+  const newProject=ref({
+    name: '',
+    description:'',
+  });
+  const createProject=async()=>{
+    console.log(newProject.value);
+
+    try{
+      const response=await fetch ('http://localhost:5000/projects',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify(newProject.value),
+      });
+
+      if(!response.ok){
+        throw new Error('Failed to create project');
+      }
+      const result =await response.json();
+      projects.value.push(result);//add new project to the last
+
+      //clear the form
+      newProject.value.name = '';
+      newProject.value.description = '';
+    }
+    catch(error){
+      console.error('Error:',error);
+    }
+  };
 </script>
 
 <style></style>
